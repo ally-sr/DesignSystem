56b8801365933d93d418bc8fde9e83a9299949c0
Ally Santana <101467403+ally-sr@users.noreply.github.com>
2023-06-01 15:45:16 -0300

    Adding tooltip

---
+++ packages/docs/src/stories/Tooltip.stories.tsx
@@ -0,0 +1,16 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { CustomTooltip  } from '@ally-igniteui/react'
+
+export default {
+    title:'Form/Tooltip',
+    component: CustomTooltip,
+
+    args:{}
+
+} as Meta
+
+export const Primary: StoryObj = {
+    args:{}
+}
+
+
